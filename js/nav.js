d9bc6ff42d25fb3405ef458f1ade81f0b89b8e66
mubaracktahir <mubarack.tahir@yahoo.com>
2022-10-15 21:23:31 +0100

    removed nav bar

--- js/nav.js
+++ js/nav.js
@@ -1,34 +1,3 @@
-class MyHeader extends HTMLElement{
-    connectedCallback(){
-        this.innerHTML = `
-        <header class="primary-header">
-            <div>
-                
-            </div>
-            <div class="logo">
-                <img src="../assets/img/OrangeMarketicon.png" alt="OrangeMarketicon">
-            </div>
-            <button class="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded="false">
-                <span class="sr-only">Menu</span>
-            </button>
-            <nav>
-                <ul id="primary-navigation"  data-visible="false" class="primary-navigation">
-                    <li><a href="">Home <span><img src="../assets/img/menu-arrow.svg" alt=""></span></a></li>
-                    <li><a href=""> About<span><img src="../assets/img/menu-arrow.svg" alt=""></span></a></li>
-                    <li><a href=""> Suppliers<span><img src="../assets/img/menu-arrow.svg" alt=""></span></a></li>
-                    <li><a href="">Resources <span><img src="../assets/img/menu-arrow.svg" alt=""></span></a></li>
-                    <li><a href="">Blog <span><img src="../assets/img/menu-arrow.svg" alt=""></span></a></li>
-                </ul>
-            </nav>
-    
-            <div class="downloadbtn">
-                <a href="#">Download the app</a>
-            </div>
-        </header>
-    `
-    }
-}
-customElements.define('my-header',MyHeader)
 const  primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 
