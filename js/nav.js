c10f76a65df2700ad08e9d5b866c3109eb570048
Abdullahi Yusuf <g.abdullahi.yusuf@gmail.com>
2022-11-07 09:56:16 -0800

    fixed the navbar

--- js/nav.js
+++ js/nav.js
@@ -1,12 +1,17 @@
-const  primaryNav = document.querySelector('.primary-navigation');
-const navToggle = document.querySelector('.mobile-nav-toggle');
-navToggle.addEventListener('click', () => {
-    const visibility = primaryNav.getAttribute("data-visible")
-    if (visibility === "false") {
-        primaryNav.setAttribute('data-visible', true);
-        navToggle.setAttribute('aria-expanded', true)
-    } else if (visibility === "true"){
-        primaryNav.setAttribute('data-visible', false)
-        navToggle.setAttribute('aria-expanded', false)
-    }
-});
+window.onload = function() {
+    window.addEventListener('scroll', function (e){
+        if (window.pageYOffset > 100) {
+            document.querySelector("header").classList.add('is-scrolling');
+        } else {
+            document.querySelector("header").classList.remove('is-scrolling');
+        }
+    });
+
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.primary-navigation');
+
+    menu_btn.addEventListener('click', function(){
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+}
