ba8a2523c550ec52f5d6dcb030139f8873364248
Abdullahi Yusuf <g.abdullahi.yusuf@gmail.com>
2022-12-02 01:03:37 -0800

    fixed the navLinks

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,4 @@
-const menu_btn = document.getElementById('hamburger');
+    const menu_btn = document.getElementById('hamburger');
     const mobile_menu = document.getElementById('primary-navigation');
     document.onclick = function(e){
         if(e.target.id !== 'primary-navigation' && e.target.id !== 'hamburger')
@@ -11,3 +11,16 @@ const menu_btn = document.getElementById('hamburger');
         menu_btn.classList.toggle('is-active');
         mobile_menu.classList.toggle('is-active');
     };
+    var links = document.querySelectorAll(".links li");
+    
+    links.forEach(li => {
+        li.addEventListener("click",()=>{
+            resetLinks();
+                li.classList.add("checked");
+        })
+    })
+    function resetLinks(){
+        links.forEach(li=> {
+            li.classList.remove("checked")
+        })
+    }
