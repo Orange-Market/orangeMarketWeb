3ccba8a68badbc5e8b0e7bfd6021b162e5a2e07b
Abdullahi Yusuf <g.abdullahi.yusuf@gmail.com>
2023-01-22 01:43:28 -0800

    fixed

--- js/testimonia.js
+++ js/testimonia.js
@@ -8,6 +8,11 @@ var sectionIndex = 0;
 leftArrow.addEventListener('click', function(){
     sectionIndex =(sectionIndex > 0) ? sectionIndex - 1 : 0;
     slider.style.transform = 'translate(' + (sectionIndex) * -33.3 + '%)';
+    // if(sectionIndex == 0){
+    //     leftArrow.style.backgroundcolor = "#FF9C41";
+    // }else{
+    //     leftArrow.style.backgroundcolor = "lightgray";
+    // }
 });
 var sectionIndex = 0;
 rightArrow.addEventListener('click', function(){
